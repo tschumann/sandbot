031899566a2577173be1bef9043f6861013b5b78
Tom Schumann <schumann.tom@gmail.com>
2016-05-06 20:21:51 +1000

    Updated weapons constants.

--- dlls/bot_weapons.h
+++ dlls/bot_weapons.h
@@ -9,113 +9,165 @@
 #ifndef BOT_WEAPONS_H
 #define BOT_WEAPONS_H
 
-// weapon ID values for Valve's Half-Life Deathmatch
-#define VALVE_WEAPON_CROWBAR       1
-#define VALVE_WEAPON_GLOCK         2
-#define VALVE_WEAPON_PYTHON        3
-#define VALVE_WEAPON_MP5           4
-#define VALVE_WEAPON_CHAINGUN      5
-#define VALVE_WEAPON_CROSSBOW      6
-#define VALVE_WEAPON_SHOTGUN       7
-#define VALVE_WEAPON_RPG           8
-#define VALVE_WEAPON_GAUSS         9
-#define VALVE_WEAPON_EGON         10
-#define VALVE_WEAPON_HORNETGUN    11
-#define VALVE_WEAPON_HANDGRENADE  12
-#define VALVE_WEAPON_TRIPMINE     13
-#define VALVE_WEAPON_SATCHEL      14
-#define VALVE_WEAPON_SNARK        15
+// weapon ID values for Half-Life
+#define VALVE_WEAPON_CROWBAR		1
+#define VALVE_WEAPON_GLOCK			2
+#define VALVE_WEAPON_PYTHON			3
+#define VALVE_WEAPON_MP5			4
+#define VALVE_WEAPON_CHAINGUN		5
+#define VALVE_WEAPON_CROSSBOW		6
+#define VALVE_WEAPON_SHOTGUN		7
+#define VALVE_WEAPON_RPG			8
+#define VALVE_WEAPON_GAUSS			9
+#define VALVE_WEAPON_EGON			10
+#define VALVE_WEAPON_HORNETGUN		11
+#define VALVE_WEAPON_HANDGRENADE	12
+#define VALVE_WEAPON_TRIPMINE		13
+#define VALVE_WEAPON_SATCHEL		14
+#define VALVE_WEAPON_SNARK			15
 
-#define VALVE_MAX_NORMAL_BATTERY   100
-#define VALVE_HORNET_MAX_CARRY      8
+#define VALVE_MAX_NORMAL_BATTERY	100
+#define VALVE_HORNET_MAX_CARRY		8
 
-
-// weapon ID values for Valve's Team Fortress Classic & 1.5
-#define TF_WEAPON_UNKNOWN1       1
-#define TF_WEAPON_UNKNOWN2       2
-#define TF_WEAPON_MEDIKIT        3
-#define TF_WEAPON_SPANNER        4
-#define TF_WEAPON_AXE            5
-#define TF_WEAPON_SNIPERRIFLE    6
-#define TF_WEAPON_AUTORIFLE      7
-#define TF_WEAPON_SHOTGUN        8
-#define TF_WEAPON_SUPERSHOTGUN   9
-#define TF_WEAPON_NAILGUN       10
-#define TF_WEAPON_SUPERNAILGUN  11
-#define TF_WEAPON_GL            12
-#define TF_WEAPON_FLAMETHROWER  13
-#define TF_WEAPON_RPG           14
-#define TF_WEAPON_IC            15
-#define TF_WEAPON_UNKNOWN16     16
-#define TF_WEAPON_AC            17
-#define TF_WEAPON_UNKNOWN18     18
-#define TF_WEAPON_UNKNOWN19     19
-#define TF_WEAPON_TRANQ         20
-#define TF_WEAPON_RAILGUN       21
-#define TF_WEAPON_PL            22
-#define TF_WEAPON_KNIFE         23
-
+// weapon ID values for Half-Life Opposing Force
+#define GEARBOX_WEAPON_CROWBAR		1
+#define GEARBOX_WEAPON_GLOCK		2
+#define GEARBOX_WEAPON_PYTHON		3
+#define GEARBOX_WEAPON_MP5			4
+#define GEARBOX_WEAPON_CHAINGUN		5
+#define GEARBOX_WEAPON_CROSSBOW		6
+#define GEARBOX_WEAPON_SHOTGUN		7
+#define GEARBOX_WEAPON_RPG			8
+#define GEARBOX_WEAPON_GAUSS		9
+#define GEARBOX_WEAPON_EGON			10
+#define GEARBOX_WEAPON_HORNETGUN	11
+#define GEARBOX_WEAPON_HANDGRENADE	12
+#define GEARBOX_WEAPON_TRIPMINE		13
+#define GEARBOX_WEAPON_SATCHEL		14
+#define GEARBOX_WEAPON_SNARK		15
+#define GEARBOX_WEAPON_GRAPPLE		16
+#define GEARBOX_WEAPON_EAGLE		17
+#define GEARBOX_WEAPON_PIPEWRENCH	18
+#define GEARBOX_WEAPON_M249			19
+#define GEARBOX_WEAPON_DISPLACER	20
+#define GEARBOX_WEAPON_UNKNOWN21	21
+#define GEARBOX_WEAPON_SHOCKRIFLE	22
+#define GEARBOX_WEAPON_SPORELAUNCHER	23
+#define GEARBOX_WEAPON_SNIPERRIFLE	24
+#define GEARBOX_WEAPON_KNIFE		25
 
 // weapon ID values for Counter-Strike
-#define CS_WEAPON_P228           1
-#define CS_WEAPON_SHIELDGUN      2
-#define CS_WEAPON_SCOUT          3
-#define CS_WEAPON_HEGRENADE      4
-#define CS_WEAPON_XM1014         5
-#define CS_WEAPON_C4             6
-#define CS_WEAPON_MAC10          7
-#define CS_WEAPON_AUG            8
-#define CS_WEAPON_SMOKEGRENADE   9
-#define CS_WEAPON_ELITE         10
-#define CS_WEAPON_FIVESEVEN     11
-#define CS_WEAPON_UMP45         12
-#define CS_WEAPON_SG550         13
-#define CS_WEAPON_GALIL         14
-#define CS_WEAPON_FAMAS         15
-#define CS_WEAPON_USP           16
-#define CS_WEAPON_GLOCK18       17
-#define CS_WEAPON_AWP           18
-#define CS_WEAPON_MP5NAVY       19
-#define CS_WEAPON_M249          20
-#define CS_WEAPON_M3            21
-#define CS_WEAPON_M4A1          22
-#define CS_WEAPON_TMP           23
-#define CS_WEAPON_G3SG1         24
-#define CS_WEAPON_FLASHBANG     25
-#define CS_WEAPON_DEAGLE        26
-#define CS_WEAPON_SG552         27
-#define CS_WEAPON_AK47          28
-#define CS_WEAPON_KNIFE         29
-#define CS_WEAPON_P90           30
+#define CS_WEAPON_P228				1
+#define CS_WEAPON_SHIELDGUN			2
+#define CS_WEAPON_SCOUT				3
+#define CS_WEAPON_HEGRENADE			4
+#define CS_WEAPON_XM1014			5
+#define CS_WEAPON_C4				6
+#define CS_WEAPON_MAC10				7
+#define CS_WEAPON_AUG				8
+#define CS_WEAPON_SMOKEGRENADE		9
+#define CS_WEAPON_ELITE				10
+#define CS_WEAPON_FIVESEVEN			11
+#define CS_WEAPON_UMP45				12
+#define CS_WEAPON_SG550				13
+#define CS_WEAPON_GALIL				14
+#define CS_WEAPON_FAMAS				15
+#define CS_WEAPON_USP				16
+#define CS_WEAPON_GLOCK18			17
+#define CS_WEAPON_AWP				18
+#define CS_WEAPON_MP5NAVY			19
+#define CS_WEAPON_M249				20
+#define CS_WEAPON_M3				21
+#define CS_WEAPON_M4A1				22
+#define CS_WEAPON_TMP				23
+#define CS_WEAPON_G3SG1				24
+#define CS_WEAPON_FLASHBANG			25
+#define CS_WEAPON_DEAGLE			26
+#define CS_WEAPON_SG552				27
+#define CS_WEAPON_AK47				28
+#define CS_WEAPON_KNIFE				29
+#define CS_WEAPON_P90				30
 
+// weapon ID values for Team Fortress Classic
+#define TF_WEAPON_UNKNOWN1			1
+#define TF_WEAPON_UNKNOWN2			2
+#define TF_WEAPON_MEDIKIT			3
+#define TF_WEAPON_SPANNER			4
+#define TF_WEAPON_AXE				5
+#define TF_WEAPON_SNIPERRIFLE		6
+#define TF_WEAPON_AUTORIFLE			7
+#define TF_WEAPON_SHOTGUN			8
+#define TF_WEAPON_SUPERSHOTGUN		9
+#define TF_WEAPON_NAILGUN			10
+#define TF_WEAPON_SUPERNAILGUN		11
+#define TF_WEAPON_GL				12
+#define TF_WEAPON_FLAMETHROWER		13
+#define TF_WEAPON_RPG				14
+#define TF_WEAPON_IC				15
+#define TF_WEAPON_UNKNOWN16			16
+#define TF_WEAPON_AC				17
+#define TF_WEAPON_UNKNOWN18			18
+#define TF_WEAPON_UNKNOWN19			19
+#define TF_WEAPON_TRANQ				20
+#define TF_WEAPON_RAILGUN			21
+#define TF_WEAPON_PL				22
+#define TF_WEAPON_KNIFE				23
 
-// weapon ID values for Gearbox's OpFor Deathmatch
-#define GEARBOX_WEAPON_CROWBAR       1
-#define GEARBOX_WEAPON_GLOCK         2
-#define GEARBOX_WEAPON_PYTHON        3
-#define GEARBOX_WEAPON_MP5           4
-#define GEARBOX_WEAPON_CHAINGUN      5
-#define GEARBOX_WEAPON_CROSSBOW      6
-#define GEARBOX_WEAPON_SHOTGUN       7
-#define GEARBOX_WEAPON_RPG           8
-#define GEARBOX_WEAPON_GAUSS         9
-#define GEARBOX_WEAPON_EGON         10
-#define GEARBOX_WEAPON_HORNETGUN    11
-#define GEARBOX_WEAPON_HANDGRENADE  12
-#define GEARBOX_WEAPON_TRIPMINE     13
-#define GEARBOX_WEAPON_SATCHEL      14
-#define GEARBOX_WEAPON_SNARK        15
-#define GEARBOX_WEAPON_GRAPPLE      16
-#define GEARBOX_WEAPON_EAGLE        17
-#define GEARBOX_WEAPON_PIPEWRENCH   18
-#define GEARBOX_WEAPON_M249         19
-#define GEARBOX_WEAPON_DISPLACER    20
-#define GEARBOX_WEAPON_UNKNOWN21    21
-#define GEARBOX_WEAPON_SHOCKRIFLE   22
-#define GEARBOX_WEAPON_SPORELAUNCHER 23
-#define GEARBOX_WEAPON_SNIPERRIFLE  24
-#define GEARBOX_WEAPON_KNIFE        25
+// weapon ID values for Gunman Chronicles
+#define GUNMAN_WEAPON_FISTS			1
+#define GUNMAN_WEAPON_GAUSSPISTOL	30
+#define GUNMAN_WEAPON_SHOTGUN		17
+#define GUNMAN_WEAPON_MINIGUN		28
+#define GUNMAN_WEAPON_BEAMGUN		14
+#define GUNMAN_WEAPON_DML			16
+#define GUNMAN_WEAPON_CHEMGUN		19
+#define GUNMAN_WEAPON_DMLGRENADE	13
+#define GUNMAN_WEAPON_AICORE		7
 
+// weapon ID values for Natural Selection
+#define NS_WEAPON_CLAWS				1
+#define NS_WEAPON_SPIT				2
+#define NS_WEAPON_SPORE				3
+#define NS_WEAPON_SPIKEGUN			4
+#define NS_WEAPON_BITEGUN			5
+#define NS_WEAPON_BITE2GUN			6
+#define NS_WEAPON_SWIPE				7
+#define NS_WEAPON_WEBSPINNER		8
+#define NS_WEAPON_METABOLIZE		9
+#define NS_WEAPON_PARASITE			10
+#define NS_WEAPON_BLINK				11
+#define NS_WEAPON_DIVINEWIND		12
+#define NS_WEAPON_KNIFE				13
+#define NS_WEAPON_PISTOL			14
+#define NS_WEAPON_MACHINEGUN		15
+#define NS_WEAPON_SHOTGUN			16
+#define NS_WEAPON_HEAVYMACHINEGUN	17
+#define NS_WEAPON_WELDER			18
+#define NS_WEAPON_MINE				19
+#define NS_WEAPON_GRENADEGUN		20
+#define NS_WEAPON_LEAP				21
+#define NS_WEAPON_CHARGE			22
+#define NS_WEAPON_UMBRA				23
+#define NS_WEAPON_PRIMALSCREAM		24
+#define NS_WEAPON_BILEBOMBGUN		25
+#define NS_WEAPON_ACIDROCKETGUN		26
+#define NS_WEAPON_HEALINGSPRAY		27
+#define NS_WEAPON_GRENADE			28
+#define NS_WEAPON_STOMP				29
+#define NS_WEAPON_DEVOUR			30
+
+// weapon ID values for They Hunger (others shared with Half-Life)
+#define HUNGER_WEAPON_TH_SPANNER	16
+#define HUNGER_WEAPON_TH_AP9		17
+#define HUNGER_WEAPON_TH_SHOVEL		18
+#define HUNGER_WEAPON_TH_SNIPER		19
+#define HUNGER_WEAPON_TH_TAURUS		21
+#define HUNGER_WEAPON_TH_CHAINGUN	22
+#define HUNGER_WEAPON_TH_MEDKIT		23
+
+// weapon ID values for The Ship
+
+// TODO
 
 // weapon ID values for FrontLineForce
 #define FLF_WEAPON_AK5           10
@@ -142,15 +194,18 @@
 
 typedef struct
 {
-   char szClassname[64];
-   int  iAmmo1;     // ammo index for primary ammo
-   int  iAmmo1Max;  // max primary ammo
-   int  iAmmo2;     // ammo index for secondary ammo
-   int  iAmmo2Max;  // max secondary ammo
-   int  iSlot;      // HUD slot (0 based)
-   int  iPosition;  // slot position
-   int  iId;        // weapon ID
-   int  iFlags;     // flags???
+	char szClassname[64];
+	int  iAmmo1;     // ammo index for primary ammo
+	int  iAmmo1Max;  // max primary ammo
+	int  iAmmo2;     // ammo index for secondary ammo
+	int  iAmmo2Max;  // max secondary ammo
+	int  iSlot;      // HUD slot (0 based)
+	int  iPosition;  // slot position
+	int  iId;        // weapon ID
+	int  iFlags;     // flags???
+	// Gunman Chronicles extras
+	char szParentClass[64];	// parent weapon for customisations
+	int iUnknown[3];		// TODO: what is this?
 } bot_weapon_t;
 
 
