3e48a14bb233fb6880f29d33b4cc7662493df456
Tom Schumann <schumann.tom@gmail.com>
2016-05-10 21:04:25 +1000

    Added more and tidied up existing message handlers.

--- dlls/bot_client.cpp
+++ dlls/bot_client.cpp
@@ -197,12 +197,6 @@ void BotClient_TFC_WeaponList(void *p, int bot_index)
 	BotClient_Valve_WeaponList(p, bot_index);
 }
 
-void BotClient_FLF_WeaponList(void *p, int bot_index)
-{
-	// this is just like the Valve Weapon List message
-	BotClient_Valve_WeaponList(p, bot_index);
-}
-
 void BotClient_Gunman_WeaponList(void *p, int bot_index)
 {
 	static int state = 0;	// current state machine state
@@ -278,6 +272,24 @@ void BotClient_Gunman_WeaponList(void *p, int bot_index)
 	}
 }
 
+void BotClient_NS_WeaponList( void *p, int bot_index )
+{
+	// this is just like the Valve Weapon List message
+	BotClient_Valve_WeaponList(p, bot_index);
+}
+
+void BotClient_Hunger_WeaponList( void *p, int bot_index )
+{
+	// this is just like the Valve Weapon List message
+	BotClient_Valve_WeaponList(p, bot_index);
+}
+
+void BotClient_FLF_WeaponList(void *p, int bot_index)
+{
+	// this is just like the Valve Weapon List message
+	BotClient_Valve_WeaponList(p, bot_index);
+}
+
 
 // This message is sent when a weapon is selected (either by the bot chosing
 // a weapon or by the server auto assigning the bot a weapon).
@@ -321,28 +333,46 @@ void BotClient_Valve_CurrentWeapon(void *p, int bot_index)
    }
 }
 
-void BotClient_TFC_CurrentWeapon(void *p, int bot_index)
+void BotClient_Gearbox_CurrentWeapon(void *p, int bot_index)
 {
-   // this is just like the Valve Current Weapon message
-   BotClient_Valve_CurrentWeapon(p, bot_index);
+	// this is just like the Valve Current Weapon message
+	BotClient_Valve_CurrentWeapon(p, bot_index);
 }
 
 void BotClient_CS_CurrentWeapon(void *p, int bot_index)
 {
-   // this is just like the Valve Current Weapon message
-   BotClient_Valve_CurrentWeapon(p, bot_index);
+	// this is just like the Valve Current Weapon message
+	BotClient_Valve_CurrentWeapon(p, bot_index);
 }
 
-void BotClient_Gearbox_CurrentWeapon(void *p, int bot_index)
+void BotClient_TFC_CurrentWeapon(void *p, int bot_index)
 {
-   // this is just like the Valve Current Weapon message
-   BotClient_Valve_CurrentWeapon(p, bot_index);
+	// this is just like the Valve Current Weapon message
+	BotClient_Valve_CurrentWeapon(p, bot_index);
+}
+
+void BotClient_Gunman_CurrentWeapon(void *p, int bot_index)
+{
+	// this is just like the Valve Current Weapon message
+	BotClient_Valve_CurrentWeapon(p, bot_index);
+}
+
+void BotClient_NS_CurrentWeapon( void *p, int bot_index )
+{
+    // this is just like the Valve Current Weapon message
+    BotClient_Valve_CurrentWeapon(p, bot_index);
+}
+
+void BotClient_Hunger_CurrentWeapon(void *p, int bot_index)
+{
+	// this is just like the Valve Current Weapon message
+	BotClient_Valve_CurrentWeapon(p, bot_index);
 }
 
 void BotClient_FLF_CurrentWeapon(void *p, int bot_index)
 {
-   // this is just like the Valve Current Weapon message
-   BotClient_Valve_CurrentWeapon(p, bot_index);
+	// this is just like the Valve Current Weapon message
+	BotClient_Valve_CurrentWeapon(p, bot_index);
 }
 
 
@@ -377,28 +407,46 @@ void BotClient_Valve_AmmoX(void *p, int bot_index)
    }
 }
 
-void BotClient_TFC_AmmoX(void *p, int bot_index)
+void BotClient_Gearbox_AmmoX(void *p, int bot_index)
 {
-   // this is just like the Valve AmmoX message
-   BotClient_Valve_AmmoX(p, bot_index);
+	// this is just like the Valve AmmoX message
+	BotClient_Valve_AmmoX(p, bot_index);
 }
 
 void BotClient_CS_AmmoX(void *p, int bot_index)
 {
-   // this is just like the Valve AmmoX message
-   BotClient_Valve_AmmoX(p, bot_index);
+	// this is just like the Valve AmmoX message
+	BotClient_Valve_AmmoX(p, bot_index);
 }
 
-void BotClient_Gearbox_AmmoX(void *p, int bot_index)
+void BotClient_TFC_AmmoX(void *p, int bot_index)
 {
-   // this is just like the Valve AmmoX message
-   BotClient_Valve_AmmoX(p, bot_index);
+	// this is just like the Valve AmmoX message
+	BotClient_Valve_AmmoX(p, bot_index);
+}
+
+void BotClient_Gunman_AmmoX(void *p, int bot_index)
+{
+	// this is just like the Valve AmmoX message
+	BotClient_Valve_AmmoX(p, bot_index);
+}
+
+void BotClient_NS_AmmoX(void *p, int bot_index)
+{
+	// this is just like the Valve AmmoX message
+	BotClient_Valve_AmmoX(p, bot_index);
+}
+
+void BotClient_Hunger_AmmoX(void *p, int bot_index)
+{
+	// this is just like the Valve AmmoX message
+	BotClient_Valve_AmmoX(p, bot_index);
 }
 
 void BotClient_FLF_AmmoX(void *p, int bot_index)
 {
-   // this is just like the Valve AmmoX message
-   BotClient_Valve_AmmoX(p, bot_index);
+	// this is just like the Valve AmmoX message
+	BotClient_Valve_AmmoX(p, bot_index);
 }
 
 
@@ -436,28 +484,46 @@ void BotClient_Valve_AmmoPickup(void *p, int bot_index)
    }
 }
 
-void BotClient_TFC_AmmoPickup(void *p, int bot_index)
+void BotClient_Gearbox_AmmoPickup(void *p, int bot_index)
 {
-   // this is just like the Valve Ammo Pickup message
-   BotClient_Valve_AmmoPickup(p, bot_index);
+	// this is just like the Valve Ammo Pickup message
+	BotClient_Valve_AmmoPickup(p, bot_index);
 }
 
 void BotClient_CS_AmmoPickup(void *p, int bot_index)
 {
-   // this is just like the Valve Ammo Pickup message
-   BotClient_Valve_AmmoPickup(p, bot_index);
+	// this is just like the Valve Ammo Pickup message
+	BotClient_Valve_AmmoPickup(p, bot_index);
 }
 
-void BotClient_Gearbox_AmmoPickup(void *p, int bot_index)
+void BotClient_TFC_AmmoPickup(void *p, int bot_index)
 {
-   // this is just like the Valve Ammo Pickup message
-   BotClient_Valve_AmmoPickup(p, bot_index);
+	// this is just like the Valve Ammo Pickup message
+	BotClient_Valve_AmmoPickup(p, bot_index);
+}
+
+void BotClient_Gunman_AmmoPickup(void *p, int bot_index)
+{
+	// this is just like the Valve Ammo Pickup message
+	BotClient_Valve_AmmoPickup(p, bot_index);
+}
+
+void BotClient_NS_AmmoPickup(void *p, int bot_index)
+{
+	// this is just like the Valve Ammo Pickup message
+	BotClient_Valve_AmmoPickup(p, bot_index);
+}
+
+void BotClient_Hunger_AmmoPickup(void *p, int bot_index)
+{
+	// this is just like the Valve Ammo Pickup message
+	BotClient_Valve_AmmoPickup(p, bot_index);
 }
 
 void BotClient_FLF_AmmoPickup(void *p, int bot_index)
 {
-   // this is just like the Valve Ammo Pickup message
-   BotClient_Valve_AmmoPickup(p, bot_index);
+	// this is just like the Valve Ammo Pickup message
+	BotClient_Valve_AmmoPickup(p, bot_index);
 }
 
 
@@ -528,46 +594,64 @@ void BotClient_Valve_Damage(void *p, int bot_index)
    }
 }
 
-void BotClient_TFC_Damage(void *p, int bot_index)
+void BotClient_Gearbox_Damage(void *p, int bot_index)
 {
-   // this is just like the Valve Battery message
-   BotClient_Valve_Damage(p, bot_index);
+	// this is just like the Valve Battery message
+	BotClient_Valve_Damage(p, bot_index);
 }
 
 void BotClient_CS_Damage(void *p, int bot_index)
 {
-   // this is just like the Valve Battery message
-   BotClient_Valve_Damage(p, bot_index);
+	// this is just like the Valve Battery message
+	BotClient_Valve_Damage(p, bot_index);
 }
 
-void BotClient_Gearbox_Damage(void *p, int bot_index)
+void BotClient_TFC_Damage(void *p, int bot_index)
 {
-   // this is just like the Valve Battery message
-   BotClient_Valve_Damage(p, bot_index);
+	// this is just like the Valve Battery message
+	BotClient_Valve_Damage(p, bot_index);
+}
+
+void BotClient_Gunman_Damage(void *p, int bot_index)
+{
+	// this is just like the Valve Battery message
+	BotClient_Valve_Damage(p, bot_index);
+}
+
+void BotClient_NS_Damage(void *p, int bot_index)
+{
+	// this is just like the Valve Battery message
+	BotClient_Valve_Damage(p, bot_index);
+}
+
+void BotClient_Hunger_Damage(void *p, int bot_index)
+{
+	// this is just like the Valve Battery message
+	BotClient_Valve_Damage(p, bot_index);
 }
 
 void BotClient_FLF_Damage(void *p, int bot_index)
 {
-   // this is just like the Valve Battery message
-   BotClient_Valve_Damage(p, bot_index);
+	// this is just like the Valve Battery message
+	BotClient_Valve_Damage(p, bot_index);
 }
 
 
 // This message gets sent when the bots money ammount changes (for CS)
 void BotClient_CS_Money(void *p, int bot_index)
 {
-   static int state = 0;   // current state machine state
+	static int state = 0;	// current state machine state
 
-   if (state == 0)
-   {
-      state++;
+	if (state == 0)
+	{
+		state++;
 
-      bots[bot_index].bot_money = *(int *)p;  // amount of money
-   }
-   else
-   {
-      state = 0;  // ingore this field
-   }
+		bots[bot_index].bot_money = *(int *)p;	// amount of money
+	}
+	else
+	{
+		state = 0;  // ingore this field
+	}
 }
 
 
@@ -615,28 +699,46 @@ void BotClient_Valve_DeathMsg(void *p, int bot_index)
    }
 }
 
-void BotClient_TFC_DeathMsg(void *p, int bot_index)
+void BotClient_Gearbox_DeathMsg(void *p, int bot_index)
 {
-   // this is just like the Valve DeathMsg message
-   BotClient_Valve_DeathMsg(p, bot_index);
+	// this is just like the Valve DeathMsg message
+	BotClient_Valve_DeathMsg(p, bot_index);
 }
 
 void BotClient_CS_DeathMsg(void *p, int bot_index)
 {
-   // this is just like the Valve DeathMsg message
-   BotClient_Valve_DeathMsg(p, bot_index);
+	// this is just like the Valve DeathMsg message
+	BotClient_Valve_DeathMsg(p, bot_index);
 }
 
-void BotClient_Gearbox_DeathMsg(void *p, int bot_index)
+void BotClient_TFC_DeathMsg(void *p, int bot_index)
 {
-   // this is just like the Valve DeathMsg message
-   BotClient_Valve_DeathMsg(p, bot_index);
+	// this is just like the Valve DeathMsg message
+	BotClient_Valve_DeathMsg(p, bot_index);
+}
+
+void BotClient_Gunman_DeathMsg(void *p, int bot_index)
+{
+	// this is just like the Valve DeathMsg message
+	BotClient_Valve_DeathMsg(p, bot_index);
+}
+
+void BotClient_NS_DeathMsg(void *p, int bot_index)
+{
+	// this is just like the Valve DeathMsg message
+	BotClient_Valve_DeathMsg(p, bot_index);
+}
+
+void BotClient_Hunger_DeathMsg(void *p, int bot_index)
+{
+	// this is just like the Valve DeathMsg message
+	BotClient_Valve_DeathMsg(p, bot_index);
 }
 
 void BotClient_FLF_DeathMsg(void *p, int bot_index)
 {
-   // this is just like the Valve DeathMsg message
-   BotClient_Valve_DeathMsg(p, bot_index);
+	// this is just like the Valve DeathMsg message
+	BotClient_Valve_DeathMsg(p, bot_index);
 }
 
 
@@ -769,28 +871,46 @@ void BotClient_Valve_ScreenFade(void *p, int bot_index)
    }
 }
 
-void BotClient_TFC_ScreenFade(void *p, int bot_index)
+void BotClient_Gearbox_ScreenFade(void *p, int bot_index)
 {
-   // this is just like the Valve ScreenFade message
-   BotClient_Valve_ScreenFade(p, bot_index);
+	// this is just like the Valve ScreenFade message
+	BotClient_Valve_ScreenFade(p, bot_index);
 }
 
 void BotClient_CS_ScreenFade(void *p, int bot_index)
 {
-   // this is just like the Valve ScreenFade message
-   BotClient_Valve_ScreenFade(p, bot_index);
+	// this is just like the Valve ScreenFade message
+	BotClient_Valve_ScreenFade(p, bot_index);
 }
 
-void BotClient_Gearbox_ScreenFade(void *p, int bot_index)
+void BotClient_TFC_ScreenFade(void *p, int bot_index)
 {
-   // this is just like the Valve ScreenFade message
-   BotClient_Valve_ScreenFade(p, bot_index);
+	// this is just like the Valve ScreenFade message
+	BotClient_Valve_ScreenFade(p, bot_index);
+}
+
+void BotClient_Gunman_ScreenFade(void *p, int bot_index)
+{
+	// this is just like the Valve ScreenFade message
+	BotClient_Valve_ScreenFade(p, bot_index);
+}
+
+void BotClient_NS_ScreenFade(void *p, int bot_index)
+{
+	// this is just like the Valve ScreenFade message
+	BotClient_Valve_ScreenFade(p, bot_index);
+}
+
+void BotClient_Hunger_ScreenFade(void *p, int bot_index)
+{
+	// this is just like the Valve ScreenFade message
+	BotClient_Valve_ScreenFade(p, bot_index);
 }
 
 void BotClient_FLF_ScreenFade(void *p, int bot_index)
 {
-   // this is just like the Valve ScreenFade message
-   BotClient_Valve_ScreenFade(p, bot_index);
+	// this is just like the Valve ScreenFade message
+	BotClient_Valve_ScreenFade(p, bot_index);
 }
 
 void BotClient_CS_HLTV(void *p, int bot_index)
