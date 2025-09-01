536d3f3cf4a5da13609b3ce7f3fb3717f962091e
tschumann <schumann.tom@gmail.com>
2025-09-01 17:21:52 +1000

    Consolidate test script builds and print what is being tested.

--- test.ps1
+++ test.ps1
@@ -12,22 +12,19 @@ Push-Location $wd
 
 # create dlls/run_tests.hxx so that test.h enables test code
 New-Item -Path "dlls" -Name "run_tests.hxx" -ItemType "file" -Force
-# build the solution
 MSBuild.exe /t:Build /p:Platform=x86 /p:Configuration=Release "sandbot.sln"
-# remove dlls/run_tests.hxx so that subsequent work doesn't get messed up by its presence
-Remove-Item dlls\run_tests.hxx
-
-# run the tests
-vstest.console.exe Release\sandbot.dll --logger:"console;verbosity=Normal"
-
-# create dlls/run_tests.hxx so that test.h enables test code
-New-Item -Path "dlls" -Name "run_tests.hxx" -ItemType "file" -Force
-# build the solution
 MSBuild.exe /t:Build /p:Platform=x64 /p:Configuration=Release "sandbot.sln"
 # remove dlls/run_tests.hxx so that subsequent work doesn't get messed up by its presence
 Remove-Item dlls\run_tests.hxx
 
-# run the tests
+Write-Output "--------------------------------"
+Write-Output "Running x86 tests"
+Write-Output "--------------------------------"
+vstest.console.exe Release\sandbot.dll --logger:"console;verbosity=Normal"
+
+Write-Output "--------------------------------"
+Write-Output "Running x64 tests"
+Write-Output "--------------------------------"
 vstest.console.exe x64\Release\sandbot.dll --logger:"console;verbosity=Normal"
 
 Pop-Location
